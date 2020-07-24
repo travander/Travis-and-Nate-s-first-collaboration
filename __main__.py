c9915a71d233a95013378ea20fe6b1a0ecdaa8b0
dojiherbs <dojiherbs@gmail.com>
2020-07-23 23:13:35 -0500

    added gmail functionality
    added gmail functionality

--- __main__.py
+++ __main__.py
@@ -1,18 +1,17 @@
 import os
 from pathlib import Path
-
-
+import ezgmail
+from datetime import date
+#pip install --user --upgrade ezgmail
 
 main_repo = "C:/Users/Nate/Desktop/travis_docs"
-
+project_location = "C:/Users/Nate/Documents/GitHub/Travis-and-Nate-s-first-collaboration"
 
 
 
 
 
 #ask for date range
-from datetime import date
-
 today = str(date.today())
 print("Today's date:", today)
 
@@ -33,7 +32,20 @@ if not os.path.isdir(main_repo + "/" + today):
 #addres:      nate.travis.testing@gmail.com
 #password:    dickbutt2
 
+os.chdir(project_location)
+ezgmail.init()
 
+
+print('Access Granted, reading emails:', '\r\n','\r\n')
+
+threads = ezgmail.search('security')
+#print(threads[0])
+
+
+ezgmail.summary(threads)
+#['tulips.jpg', 'canal.jpg', 'bicycles.jpg']
+#threads[0].messages[0].downloadAttachment('tulips.jpg')
+#threads[0].messages[0].downloadAllAttachments(downloadFolder='vacation2019')
 
 
 #parse emails/download attachments
