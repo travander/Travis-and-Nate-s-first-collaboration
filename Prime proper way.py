a1a133245027f9c0036cba128775a704f3dac6c4
travander <68717227+travander@users.noreply.github.com>
2020-07-23 22:24:14 -0500

    Add files via upload

---
+++ Prime proper way.py
@@ -0,0 +1,74 @@
+print('hi')
+
+print('my name is jeff')
+
+print ('my purpose is to tell you if a number is prime') 
+
+print('what number would you like to test') 
+
+
+mynumber = input()
+myinterger = int(mynumber)
+
+nice = 69 - myinterger
+
+int(nice)
+
+print('Could you add ' + str(nice) + ' to that please?')
+
+if int(input()) == 69:
+    print('Nice.')
+else:
+    print('you fucking idiot')
+
+input()
+
+print('so you want to know if ' + mynumber + ' is prime?')
+
+input()
+
+print('hmm let me see')
+
+myinterger = abs(myinterger)
+
+print(myinterger)
+
+list_a = list(range(1,int(myinterger/2 + 1)))
+list_b = list(range(1,int(myinterger/2 + 1)))
+
+
+
+for i in range (1,int((myinterger/2))):
+    list_a[i] = myinterger % list_a[i]
+
+
+
+
+
+
+if 0 in list_a:
+    print('this is not a prime number')
+    
+else:
+    print('This is a prime number')
+
+
+
+
+
+
+
+'''
+count = 0
+#for i in list_a:
+    if i == 0:
+        print('This is a not a prime number')
+        break
+    
+
+
+
+    
+    
+if myinterger == 1 or myinterger == 0:
+    print('This is a prime number')'''
