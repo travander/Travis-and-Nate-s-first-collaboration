6e52907746777848e26546ef86f2e04d2b8d5ea5
dojiherbs <dojiherbs@gmail.com>
2020-07-23 22:54:31 -0500

    main dump
    added date and create repo logic

--- __main__.py
+++ __main__.py
@@ -1,3 +1,40 @@
-#test
+import os
+from pathlib import Path
 
-#testing 3
+
+
+main_repo = "C:/Users/Nate/Desktop/travis_docs"
+
+
+
+
+
+
+#ask for date range
+from datetime import date
+
+today = str(date.today())
+print("Today's date:", today)
+
+
+
+
+#create repository
+if not os.path.isdir(main_repo):
+    os.makedirs(main_repo)
+os.chdir(main_repo)
+
+if not os.path.isdir(main_repo + "/" + today):
+    os.makedirs(main_repo+"/"+today)
+
+
+
+#navigate to emails
+#addres:      nate.travis.testing@gmail.com
+#password:    dickbutt2
+
+
+
+
+#parse emails/download attachments
+
